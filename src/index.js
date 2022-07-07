1dbfd9a2690a66542b99ca2b20c2a0ab1239e8c9
Mark Antonenko <2002mark.a@gmail.com>
2022-07-07 20:10:01 +0300

    finaly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import search from './js/searchFn';
-import template from './partials/picture-card';
-
 
 const button = document.querySelector('.search-btn');
 const ul = document.querySelector('.gallery');
@@ -17,7 +15,37 @@ button.addEventListener('click', e => {
   currentPage += 1;
 
   search(inputText, currentPage).then(res => {
-    ul.insertAdjacentHTML('beforeend', template(res.hits));
+    res.hits.forEach(el => {
+      ul.insertAdjacentHTML(
+        'beforeend',
+
+        `<li class='li-js'>
+        <div class='photo-card'>
+          <img src=${el.webformatURL} alt='' />
+    
+          <div class='stats'>
+            <p class='stats-item'>
+              <i class='material-icons'>thumb_up</i>
+              ${el.likes}
+            </p>
+            <p class='stats-item'>
+              <i class='material-icons'>visibility</i>
+              ${el.views}
+            </p>
+            <p class='stats-item'>
+              <i class='material-icons'>comment</i>
+              ${el.comments}
+            </p>
+            <p class='stats-item'>
+              <i class='material-icons'>cloud_download</i>
+              ${el.downloads}
+            </p>
+          </div>
+        </div>
+      </li>`
+      );
+    });
+
     if (currentPage > 1) {
       ul.lastElementChild.scrollIntoView({
         block: 'start',
@@ -35,7 +63,36 @@ loadMoreBtn.addEventListener('click', () => {
   const inputText = document.querySelector('.search-input').value;
   currentPage += 1;
   search(inputText, currentPage).then(res => {
-    ul.insertAdjacentHTML('beforeend', template(res.hits));
+    res.hits.forEach(el => {
+      ul.insertAdjacentHTML(
+        'beforeend',
+
+        `<li class='li-js'>
+        <div class='photo-card'>
+          <img src=${el.webformatURL} alt='' />
+    
+          <div class='stats'>
+            <p class='stats-item'>
+              <i class='material-icons'>thumb_up</i>
+              ${el.likes}
+            </p>
+            <p class='stats-item'>
+              <i class='material-icons'>visibility</i>
+              ${el.views}
+            </p>
+            <p class='stats-item'>
+              <i class='material-icons'>comment</i>
+              ${el.comments}
+            </p>
+            <p class='stats-item'>
+              <i class='material-icons'>cloud_download</i>
+              ${el.downloads}
+            </p>
+          </div>
+        </div>
+      </li>`
+      );
+    });
   });
 
   UpBtn.classList.remove('is-hiden');
